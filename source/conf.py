dc160a78e1992964104eb5d2af6391e0ec623a05
Anupam Maurya <anupammaurya6767@gmail.com>
2023-11-11 21:38:20 +0530

    updates

--- source/conf.py
+++ source/conf.py
@@ -6,10 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Kavy'
+project = 'Kanao'
 copyright = '2023, Anupam Maurya'
 author = 'Anupam Maurya'
-release = '1.0.3'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
